9e26d9bca3523c3743aa9e4e5b0e7fc09b82d747
wiesa56 <contact@wanwari.ca>
2021-04-30 21:52:34 -0400

    feat: implement Footer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer/Footer";
 import Tabs from "./components/Tabs/Tabs";
 
 function App() {
   return (
     <div>
       <Tabs />
+      <Footer />
     </div>
   );
 }
