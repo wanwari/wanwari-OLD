bdb08c906cf6cd515acbd1223ae5db62f702bb8e
wiesa56 <contact@wanwari.ca>
2021-04-30 22:21:04 -0400

    style: styled Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,13 @@
 const Footer = () => {
 
+  const styles={
+    container: "bg-gray-300 p-6 text-right",
+    text: "lowercase"
+  }
+
   return(
-    <div>
-      Footer
+    <div className={styles.container}>
+      <p className={styles.text}>&copy; 2021 Wiesa Anwari</p>
     </div>
   )
 }
