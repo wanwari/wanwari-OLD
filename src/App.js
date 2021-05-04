8c3cc1750495237855c94fc6d3a5caab91df0c6d
wiesa56 <contact@wanwari.ca>
2021-05-03 22:57:05 -0400

    feat: implement AddressBar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer/Footer";
 import Search from "./components/Search/Search";
 import Tabs from "./components/Tabs/Tabs";
+import AddressBar from "./components/AddressBar/AddressBar";
 
 const App = () => {
 
@@ -12,6 +13,7 @@ const App = () => {
     <div className={style.container} style={{backgroundColor: "#f96672"}} >
       <div>
         <Tabs active={0}/>
+        <AddressBar />
       </div>
       <Search />
       <Footer />
---
+++ src/components/AddressBar/AddressBar.js
@@ -0,0 +1,50 @@
+import {LeftSvg, RightSvg, RefreshSvg, HomeSvg, MenuSvg} from './Svg';
+
+const AddressBar = () => {
+
+  const refreshPage = () => {
+    window.location.reload();
+  }
+
+  const style = {
+    container: "flex justify-between bg-white py-2",
+    navBtn: "mr-1 py-2",
+    urlBar: "flex-grow rounded-lg border border-gray-300 bg-white pl-2 py-1 ml-2 text-gray-600",
+    btnGroup: "pl-4",
+    menuBtn: "mt-2 px-4",
+  };
+
+  return(
+    <div className={style.container}>
+
+      <div className={style.btnGroup}>
+        <button className={style.navBtn}>
+          <LeftSvg />
+        </button> 
+
+        <button className={style.navBtn}>
+          <RightSvg />
+        </button> 
+
+        <button className={style.navBtn} onClick={refreshPage}>
+          <RefreshSvg />
+        </button> 
+
+        <button className={style.navBtn}>
+          <HomeSvg />
+        </button> 
+      </div>
+
+      <input type="text" value="https://wanwari.ca/" className={style.urlBar} disabled />
+      
+      <div className={style.menuBtn}>
+        <button>
+          <MenuSvg />
+        </button> 
+      </div>
+
+    </div>
+  );
+}
+
+export default AddressBar;
