208cfa626c5fcc28cd9e0e17fdcb0cd101473c72
Wiesa Anwari <wiesa@wanwari.ca>
2022-06-27 11:48:39 -0400

    feat: display all information in Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,38 +1,52 @@
-const Card = props => {
+const Card = (props) => {
+	const style = {
+		card: "flex flex-col justify-between bg-white rounded-lg hover:shadow-2xl p-4 mx-8 my-8",
+		cardHeader: "text-red-500 font-bold text-2xl mt-2 mb-4",
+		cardBody: "border rounded-lg p-2",
+		desc: "p-4",
+		img: "p-4",
+		cardTags: "text-red-400 text-right font-light italic pt-3",
+		buttonGroup: "mt-4 text-center align-text-bottom",
+		cardButtons:
+			"bg-red-400 rounded-lg p-2 text-white hover:bg-red-700 mr-2",
+	};
 
-  const style = {
-    card: "flex flex-col justify-between bg-white rounded-lg hover:shadow-2xl p-4 mx-8 my-8",
-    cardHeader: "text-red-500 font-bold text-2xl mt-2 mb-4",
-    cardBody: "border rounded-lg p-2",
-    cardTags: "text-red-400 text-right font-light italic pt-3",
-    buttonGroup: "mt-4 text-center align-text-bottom",
-    cardButtons: "bg-red-400 rounded-lg p-2 text-white hover:bg-red-700 mr-2",
-  };
+	return (
+		<div className={style.card}>
+			<div>
+				<div className={style.cardHeader}>{props.projectTitle}</div>
 
-  return(
-      <div className={style.card}>
+				<div className={style.cardBody}>
+					<div className={style.desc}>{props.description}</div>
+					<img
+						className={style.img}
+						src={props.imgPath}
+						alt={props.projectTitle}
+					/>
+					<div className={style.cardTags}>{props.tags}</div>
+				</div>
+			</div>
 
-        <div>
-          <div className={style.cardHeader}>
-            Project Title
-          </div>
-
-          <div className={style.cardBody}>
-            {props.text}
-            <div className={style.cardTags}>
-              React, Tailwind, JS, HTML, CSS
-            </div>
-          </div>
-        </div>
-
-        <div className={style.buttonGroup}>
-          <button className={style.cardButtons}>View Code</button>
-          <button className={style.cardButtons}>Visit Project</button>
-        </div>
-      </div>
-  );
-
-}
+			<div className={style.buttonGroup}>
+				<a
+					href={props.codeURL}
+					target="_blank"
+					rel="noreferrer"
+					className={style.cardButtons}
+				>
+					View Code
+				</a>
+				<a
+					href={props.projectURL}
+					target="_blank"
+					rel="noreferrer"
+					className={style.cardButtons}
+				>
+					Visit Project
+				</a>
+			</div>
+		</div>
+	);
+};
 
 export default Card;
-
