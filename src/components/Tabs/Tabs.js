65453f6d1c85d031c0b6b6a51b5fa4f63a5b9499
Wiesa Anwari <wiesa@wanwari.ca>
2022-06-27 21:06:26 -0400

    feat: remove bottom bar under tabs

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,58 +1,62 @@
-import {useEffect} from "react";
-import {useHistory} from "react-router-dom";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
-const Tabs = props => {
+const Tabs = (props) => {
+	let history = useHistory();
 
-  let history = useHistory();
+	useEffect(() => {
+		const el = document.querySelector(".chrome-tabs");
+		let ChromeTabs = window.ChromeTabs;
+		let chromeTabs = new ChromeTabs();
 
-  useEffect(() => {
+		chromeTabs.init(el, history);
 
-    const el = document.querySelector('.chrome-tabs');
-    let ChromeTabs = window.ChromeTabs;
-    let chromeTabs = new ChromeTabs();
+		setTimeout(() => {
+			chromeTabs.addTab(
+				{
+					title: "Home",
+					favicon: false,
+				},
+				{
+					animate: true,
+					background: props.active === 0 ? false : true,
+				}
+			);
+		}, 100);
 
-    chromeTabs.init(el, history);
+		setTimeout(() => {
+			chromeTabs.addTab(
+				{
+					title: "Portfolio",
+					favicon: false,
+				},
+				{
+					animate: true,
+					background: props.active === 1 ? false : true,
+				}
+			);
+		}, 400);
 
-    setTimeout(() => {
-      chromeTabs.addTab({
-        title: 'Home',
-        favicon: false
-      }, {
-        animate: true,
-        background: props.active === 0 ? false : true
-      });
-    }, 100);
+		setTimeout(() => {
+			chromeTabs.addTab(
+				{
+					title: "Contact",
+					favicon: false,
+				},
+				{
+					animate: true,
+					background: props.active === 2 ? false : true,
+				}
+			);
+		}, 700);
+	}, [history, props.active]);
 
-    setTimeout(() => {
-      chromeTabs.addTab({
-        title: 'Portfolio',
-        favicon: false
-      }, {
-        animate: true,
-        background: props.active === 1 ? false : true
-      });
-    }, 400);
-
-    setTimeout(() => {
-      chromeTabs.addTab({
-        title: 'Contact',
-        favicon: false
-      }, {
-        animate: true,
-        background: props.active === 2 ? false : true
-      });
-      
-    }, 700);
-
-  }, [history, props.active]);
-
-  return(
-    <div className="chrome-tabs">
-      <div className="chrome-tabs-content"></div>
-      <div className="chrome-tabs-bottom-bar"></div>
-    </div>
-  );
-
-}
+	return (
+		<div className="chrome-tabs">
+			<div className="chrome-tabs-content"></div>
+			{/* <div className="chrome-tabs-bottom-bar"></div> */}
+		</div>
+	);
+};
 
 export default Tabs;
